ca62fdf9304844a295f84f94fbe39923acaa65d5
Jake <jtb1137@gmail.com>
2017-09-20 15:41:09 -0400

    setup show/edit routes and views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,9 +8,26 @@ class ListsController < ApplicationController
 		erb :'/lists/new'
 	end
 
+	get '/lists/:id/edit' do
+		@list = List.find_by(id: params[:id])
+		erb :'/lists/edit'
+	end
+	
+	get '/lists/:id' do
+		@list = List.find_by(id: params[:id])
+		erb :'/lists/show'
+	end
+
 	post '/lists' do
 		@list = List.create(name: params[:name])
 		@list.save
 		redirect '/lists' #make show page?
+	end
+
+	patch '/lists/:id' do
+		@list = List.find_by(id: params[:id])
+		@list.name = params[:name]
+		@list.save
+		redirect "/lists"
 	end
 end
